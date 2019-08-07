5efe468b68be2cdac53ee5d26175cedab3f9de7b
JPabloMF <juanpablo45671@gmail.com>
2019-08-07 12:02:47 -0500

    adding dashboard module to app

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import SignIn from './screens/signin';
 import SignUp from './screens/signup';
 import PageNotFound from './components/pageNotFound';
+import Dashboard from './screens/dashboard';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route exact path="/" component={SignIn} />
         <Route path="/signin" component={SignIn} />
         <Route path="/signup" component={SignUp} />
+        <Route path="/dashboard" component={Dashboard} />
         <Route path="" component={PageNotFound} />
       </Switch>
     </Router>
